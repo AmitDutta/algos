2e3c0f1be70808a0a4095958df1dfbe37212e4db
Amit Dutta <amit.856@gmail.com>
2015-06-27 15:11:54 -0700

    Adding few more

--- src/ds/TreeCodes.java
+++ src/ds/TreeCodes.java
@@ -24,11 +24,129 @@ public class TreeCodes {
       }
       return right;
    }
+   
+   private int diameter = 0;
+   
+   private int diameterInt(TreeNode root) {
+      if (root == null) {
+         return 0;
+      }
+      int left = diameterInt(root.left);
+      int right = diameterInt(root.right);
+      int val = 1 + left + right;
+      diameter = Math.max(diameter, val);
+      return 1 + Math.max(left, right);
+   }
+   
+   public int diameter(TreeNode root) {
+      diameter = 0;
+      diameterInt(root);
+      return diameter;
+   }
+
    // http://www.danielbit.com/blog/puzzle/leetcode/leetcode-binary-tree-upside-down
    public TreeNode flip(TreeNode root) {
       flipInt(root);
       return result;
    }
+
+   @Test
+   public void diameterTest1() {
+      TreeNode node1 = new TreeNode(1);
+      Assert.assertEquals(1, diameter(node1));
+      TreeNode node2 = new TreeNode(2);
+      TreeNode node3 = new TreeNode(3);
+      node1.left = node2;
+      node1.right = node3;
+      Assert.assertEquals(3, diameter(node1));
+   }
+   
+   @Test
+   public void diameterTest2() {
+      TreeNode node1 = new TreeNode(1);
+      TreeNode node2 = new TreeNode(2);
+      TreeNode node3 = new TreeNode(3);
+      TreeNode node4 = new TreeNode(4);
+      TreeNode node5 = new TreeNode(5);
+      TreeNode node6 = new TreeNode(6);
+      TreeNode node7 = new TreeNode(7);
+      TreeNode node8 = new TreeNode(8);
+      TreeNode node9 = new TreeNode(9);
+      TreeNode node10 = new TreeNode(10);
+      TreeNode node11 = new TreeNode(11);
+      TreeNode node12 = new TreeNode(12);
+      TreeNode node13 = new TreeNode(13);
+      
+      node1.left = node2;
+      node1.right = node3;
+      
+      node2.left = node4;
+      node2.right = node5;
+      
+      node5.left = node7;
+      node5.right = node8;
+      
+      node3.right = node6;
+      
+      node6.right = node9;
+      
+      node9.left = node10;
+      
+      node10.left = node12;
+      node10.right = node13;
+      
+      node9.right = node11;
+      
+      /*System.out.println("--");
+      TreeNode.toLevelOrderPrint(node1);*/
+      
+      Assert.assertEquals(9, diameter(node1));
+   }
+   
+   @Test
+   public void diametertest3() {
+      TreeNode node1 = new TreeNode(1);
+      TreeNode node2 = new TreeNode(2);
+      TreeNode node3 = new TreeNode(3);
+      TreeNode node4 = new TreeNode(4);
+      TreeNode node5 = new TreeNode(5);
+      TreeNode node6 = new TreeNode(6);
+      TreeNode node7 = new TreeNode(7);
+      TreeNode node8 = new TreeNode(8);
+      TreeNode node9 = new TreeNode(9);
+      TreeNode node10 = new TreeNode(10);
+      TreeNode node11 = new TreeNode(11);
+      TreeNode node12 = new TreeNode(12);
+      TreeNode node13 = new TreeNode(13);
+      TreeNode node14 = new TreeNode(14);
+      TreeNode node15 = new TreeNode(15);
+      
+      node1.left = node2;
+      node1.right = node3;
+      
+      node2.left = node4;
+      node2.right = node5;
+      
+      node3.right = node6;
+      
+      node4.left = node7;
+      node4.right = node8;
+      
+      node8.left = node10;
+      node10.left = node13;
+      node10.right = node14;
+      
+      node5.right = node9;
+      node9.left = node11;
+      node9.right = node12;
+      node12.right = node15;
+      
+      /*System.out.println("--");
+      TreeNode.toLevelOrderPrint(node1);*/
+      
+      Assert.assertEquals(9, diameter(node1));
+   }
+   
    @Test
    public void test1() {
       TreeNode node1 = new TreeNode(1);
@@ -65,12 +183,12 @@ public class TreeCodes {
       node1.right = node3;
       node2.left = node4;
       node2.right = node5;
-      TreeNode.printAllPaths(node1);
+      //TreeNode.printAllPaths(node1);
    }
    
    @Test
    public void test3() {
       TreeNode node = TreeNode.FromLevelOrderArray(new String[] {"1","2","3","#","#","#","#"});
-      TreeNode.toLevelOrderPrint(node);
+      //TreeNode.toLevelOrderPrint(node);
    }
 }
--- src/misc/SlidingWindow.java
+++ src/misc/SlidingWindow.java
@@ -56,4 +56,61 @@ public class SlidingWindow {
       Assert.assertEquals(5, lengthOfLongestSubstring("abcdbcade"));
       Assert.assertEquals(0, lengthOfLongestSubstring(""));
    }
+   
+   // leetcode prob 76..best sliding window example
+   public String minWindow(String s, String t) {
+      Map<Character, Integer> map = new HashMap<Character, Integer>();
+      for (int i = 0; i < t.length(); ++i) {
+         char ch = t.charAt(i);
+         if (map.containsKey(ch)) {
+            map.put(t.charAt(i), map.get(ch) + 1);
+         } else {
+            map.put(ch, 1);
+         }
+      }
+      int len = 0, start = 0;
+      String result = "";
+      Map<Character, Integer> tmp = new HashMap<Character, Integer>();
+      for (int i = 0; i < s.length(); ++i) {
+         char ch = s.charAt(i);
+         if (!map.containsKey(ch)) {
+            continue;
+         }
+         if (tmp.containsKey(ch)) {
+            tmp.put(ch, tmp.get(ch) + 1);
+         } else {
+            tmp.put(ch, 1);
+         }
+         if (tmp.get(ch) <= map.get(ch)) {
+            len++;
+         }
+         if (len >= t.length()) {
+            while (start <= i) {
+               char ch2 = s.charAt(start);
+               if (!map.containsKey(ch2)) {
+                  start++;
+                  continue;
+               }
+               if (tmp.get(ch2) > map.get(ch2)) {
+                  tmp.put(ch2, tmp.get(ch2) - 1);
+                  start++;
+               } else {
+                  break;
+               }
+            }
+            if (result == "" || result.length() > i - start) {
+               result = s.substring(start, i + 1);
+            }
+         }
+      }
+      return result;
+   }
+   
+   @Test
+   public void test2() {
+      Assert.assertEquals("BANC", minWindow("ADOBECODEBANC", "ABC"));
+      Assert.assertEquals("cba", minWindow("abbcbcba", "abc"));
+      Assert.assertEquals("aa", minWindow("aa", "aa"));
+      Assert.assertEquals("ba", minWindow("bba", "ab"));
+   }
 }
