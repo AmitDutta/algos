d95be37584e619e057687c971272193c636e4abc
Amit Dutta <amit.856@gmail.com>
2015-08-02 22:54:13 -0700

    Adding few

--- src/leetcodeoj/Problem6.java
+++ src/leetcodeoj/Problem6.java
@@ -1,12 +1,44 @@
 package leetcodeoj;
 import  org.junit.Assert;
-
 import org.junit.Test;
 
 public class Problem6 {
-    
+   public String convert(String s, int numRows) {
+      StringBuffer result = new StringBuffer();
+      StringBuffer[] buffers = new StringBuffer[numRows];
+      for (int i = 0; i < buffers.length; ++i) {
+         buffers[i] = new StringBuffer();
+      }
+      boolean shrink = false;
+      int start = 0, end = numRows - 1;
+      for (int i = 0; i < s.length(); ++i) {
+         buffers[start].append(s.charAt(i));
+         if (start == end) {
+            if (shrink) {
+               start = 0;
+               end = numRows - 1;
+               shrink = !shrink;
+            } else {
+               if (numRows > 2) {
+                  start = numRows - 2;
+                  end = 1;
+                  shrink = !shrink;
+               } else {
+                  start = 0;
+                  end = numRows - 1;
+               }
+            }
+         } else {
+            start = shrink ? start - 1 : start + 1;
+         }
+      }
+      for (int i = 0; i < buffers.length; ++i) {
+         result.append(buffers[i]);
+      }
+      return result.toString();
+   }
     // Without using extra memory: http://tech-wonderland.net/blog/leetcode-zigzag-conversion.html
-    public String convert(String s, int nRows) {
+    public String convert1(String s, int nRows) {
         int i = 0;
         StringBuffer[] buffers = new StringBuffer[nRows];
         for (i = 0; i < nRows; ++i) {
